2c304361ff62e6d84801dfef2e77d33bb34c0fb4
Dan <danhemed123@gmail.com>
2025-06-09 17:52:04 +0300

    add a file of class IntelReportsDAL

--- Program.cs
+++ Program.cs
@@ -8,9 +8,13 @@ namespace Malshinon.Models
         public static void Main(string[] args)
         {
             MySqlData sqlData = new MySqlData();
-            People people = new People(1, "firstName", "lastName", "people1", "reporter", 0 , 0 );
-            PeopleDAL peopleDAL = new PeopleDAL(sqlData);
-            peopleDAL.AddPeople(people);
+            // People people = new People(2, "firstName", "lastName", "people1", "reporter", 0, 0);
+            // PeopleDAL peopleDAL = new PeopleDAL(sqlData);
+            // peopleDAL.AddPeople(people);
+
+            IntelReports reports = new IntelReports(1, 1, 1, "Report", $"{DateTime.Now}");
+            IntelReportsDAL reportsDAL = new IntelReportsDAL(sqlData);
+            reportsDAL.AddReports(reports);
         }
     }
 } 
