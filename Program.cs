56527fcde6210532e73e1dc492d82a0561741689
Dan <danhemed123@gmail.com>
2025-06-11 11:58:30 +0300

    .

--- Program.cs
+++ Program.cs
@@ -8,35 +8,6 @@ namespace Malshinon.Models
         public static void Main(string[] args)
         {
             MySqlData sqlData = new MySqlData();
-            People people = new People { FirstName = "F3", LastName = "L3", SecretCode = "19B", Type = "reporter" };
-            PeopleDAL peopleDAL = new PeopleDAL(sqlData);
-            peopleDAL.AddPeople(people);
-
-            // # for print the table people #
-            foreach (var person in peopleDAL.GetAllPeople())
-            {
-                Console.WriteLine(person);
-            }
-
-            Console.WriteLine("PERSON!!");
-            Console.WriteLine(peopleDAL.GetPerson("19B"));
-            
-            peopleDAL.UpdatePerson("name", "name", "45t", "potential_agent");
-            peopleDAL.UpdateReportsNum("45t");
-            peopleDAL.UpdateMentionsNum("45t");
-            peopleDAL.DeletePerson("19B");
-
-
-            // # for print the table people #
-            foreach (var person in peopleDAL.GetAllPeople())
-            {
-                Console.WriteLine(person);
-            }
-
-
-            // IntelReports reports = new IntelReports(id: 1, reporterId: 1, targetId: 2, text: "Report", timeStamp: DateTime.Now);
-            // IntelReportsDAL reportsDAL = new IntelReportsDAL(sqlData);
-            // reportsDAL.AddReports(reports);
         }
     }
 } 
